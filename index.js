43a048fe1185092a15d680ea7fa7fb485e23ea6a
Alysson-b <161394750+Alysson-b@users.noreply.github.com>
2024-05-28 22:11:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-let pergunta1 = prompt('telefonou para a vitima?')
-let pergunta2 = prompt('esteve no local do crime')
-let pergunta3 = prompt('mora perto da vitima')
-let pergunta4 = prompt('devia para a vitima')
-let pergunta5 = prompt('ja trabalhou com a vitima')
+function perguntar(){
+  let perguntas = [ 
+  'telefonou para a vitima?',
+  'esteve no local do crime?' ,
+  'mora perto da vitima?',
+  'devia para a vitima?',
+  'ja trabalhou com a vitima?'
+
+]
+  let respostas = [];
+
+ for(let pergunta of perguntas){
+  let resposta = prompt(pergunta + " (S/N)").toUpperCase();
+  while(resposta !== 'S' && resposta !== 'N'){
+    alert('resposta invalida, responda com S ou N.')
+    resposta = prompt(pergunta + "(S/N)").toUpperCase();
+  }
+  respostas.push(resposta)
+ }
+ 
+ return respostas;
+}
 
 
-if(prompt < 2){
-    console.log('suspeito')
+
+function veredito(respostas) {
+  let respostasPositivas = respostas.filter(resposta => resposta === 'S').length;
+
+  if(respostasPositivas === 2 ){
+    return('Supeito')
+  }else if(respostasPositivas >=3 && respostasPositivas <=4){
+    return('Cúmplice')
+  }else if(respostasPositivas === 5){
+    return('Assasino')
+  }else{
+    return('inocente')
+  }
+} 
+
+function main() {
+  let respostas = perguntar();
+  let resultado = veredito(respostas);
+  alert('Voce é:' +  resultado);
 }
-else if(prompt >= 3  &&  prompt <= 4){
-  console.log('Cúmplice')
-}
-else{
-  console.log('assasino')
-}
+
+
+main();
 
